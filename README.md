e5541c9531cefa3610b9dfb13cf0d5b796ab73c0
Remi <97021497+Remi-1201@users.noreply.github.com>
2022-07-06 18:34:06 +0900

    axios でデータを取得してみる

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# React-app08
-Created with CodeSandbox
+## [Udemy-React に入門した人のためのもっと React が楽しくなるステップアップコース完全版](https://www.udemy.com/course/react_stepup/learn/lecture/25224322#search)
+
+### Section 8 - JSONPlaceholder から axios でデータを取得してみる
+
+### Links
+
+- [成果物](https://ki7v1f.csb.app/)
+- [JSONPlaceholder](https://jsonplaceholder.typicode.com/)
--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+// add axios
+import axios from "axios";
 import "./styles.css";
 
 export default function App() {
+  const onClickUsers = () => {
+    // then = データを取得した後に実行する関数
+    // 引数resに取得したデータの情報が入る
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+        // catch = 何かエラーが発生した時に実行する関数
+      })
+      .catch((err) => console.log(err));
+  };
+  const onClickUser1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users?id=1")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>user with id=1</button>
     </div>
   );
 }
